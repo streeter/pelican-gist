dc54368f65e476974e87b69d3a67786ecce59456
Chris Streeter <chris@chrisstreeter.com>
2013-11-26 11:16:21 -0800

    Version bump

--- pelican_gist/__init__.py
+++ pelican_gist/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __title__ = 'pelican-gist'
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __author__ = 'Chris Streeter'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2013'
