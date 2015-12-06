d8205680a91dbb158de17f38a182693f5af7691c
Chris Streeter <chris@chrisstreeter.com>
2015-12-06 09:00:09 -0800

    Update copyright date

--- pelican_gist/__init__.py
+++ pelican_gist/__init__.py
@@ -3,7 +3,7 @@ __title__ = 'pelican-gist'
 __version__ = '0.3.3'
 __author__ = 'Chris Streeter'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2013'
+__copyright__ = 'Copyright 2015'
 
 
 from pelican_gist.plugin import register
