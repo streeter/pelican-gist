395e619534b404fb2b94456dc400dc2a8a2f934a
Chris Streeter <chris@chrisstreeter.com>
2018-01-14 22:06:00 -0800

    Fix classifiers

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     install_requires=requires,
     tests_require=tests_require,
     license='MIT',
-    classifiers=(
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
@@ -57,8 +57,9 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing',
-    ),
+    ],
 )
